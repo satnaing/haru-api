6585695800fe5565e960cc23262c624b48a9c5ac
Sat Naing <satnaingdev@gmail.com>
2021-09-26 21:37:53 +0630

    Hello World

---
+++ app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("tiny"));
+
+app.get("/hello", (req, res) => {
+  res.status(200).json({
+    message: "Hello World",
+  });
+});
+
+export default app;
