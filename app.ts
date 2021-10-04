13a84deefaf35541c2c39588e6deb06773a4fd13
Sat Naing <satnaingdev@gmail.com>
2021-10-05 00:47:05 +0630

    :sparkles: setup auth route,controller and test

--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ import errorHandler from "./middlewares/errorHandler";
 import categories from "./routers/categories";
 import products from "./routers/products";
 import customers from "./routers/customers";
+import auth from "./routers/auth";
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/categories", categories);
 app.use("/api/v1/products", products);
 app.use("/api/v1/customers", customers);
+app.use("/api/v1/auth", auth);
 
 app.use(errorHandler);
 
---
+++ routers/auth.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/register").post("/login");
+
+export default router;
