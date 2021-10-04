1562e28aea133968c609343daeffd1d86e823986
Sat Naing <satnaingdev@gmail.com>
2021-10-04 23:10:39 +0630

    :wrench: update compose name, script cmd and add jest-extended

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   clearMocks: true,
   preset: "ts-jest",
   testEnvironment: "node",
-  setupFilesAfterEnv: ["jest-sorted"],
+  setupFilesAfterEnv: ["jest-sorted", "jest-extended"],
   // testTimeout: 20000,
   // setupFilesAfterEnv: ["<rootDir>/prisma/singleton.ts"],
 };
